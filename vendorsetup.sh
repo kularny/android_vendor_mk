f18067b269bf647cb761d6996300b5ee8de1ee2b
Arnab Chaudhuri <coolguyforgenerationx@gmail.com>
2015-05-07 03:27:11 +0530

    added i9100 in lunch combo

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,1 @@
-for combo in $(curl -s https://raw.githubusercontent.com/MoKee/android_external_mokee_BuildController/mkl-mr1/mk_build_targets | sed -e 's/#.*$//' | awk {'print $1'})
-do
-    add_lunch_combo $combo
-done
+add_lunch_combo mk_i9100-userdebug
